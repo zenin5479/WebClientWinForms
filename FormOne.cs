7ad5a95b26dfff2be864c7b30a38508462209f21
zenin5479 <zenin5479@gmail.com>
2025-11-28 23:03:12 +0300

    Update 0.8 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -10,8 +10,8 @@ namespace WebClientWinForms
       {
          InitializeComponent();
 
-         WebClient Client = new WebClient();
-         Stream strm = Client.OpenRead("http://www.reuters.com");
+         WebClient client = new WebClient();
+         Stream strm = client.OpenRead("http://www.reuters.com");
          StreamReader sr = new StreamReader(strm);
          string line;
          while ((line = sr.ReadLine()) != null)
