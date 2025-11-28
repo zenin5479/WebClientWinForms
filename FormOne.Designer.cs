77779f10dda8d1a5c952f6ba0410c39acfedd1b2
zenin5479 <zenin5479@gmail.com>
2025-11-28 17:00:02 +0300

    Update 0.4 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -28,10 +28,17 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.components = new System.ComponentModel.Container();
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(800, 450);
-         this.Text = "Form1";
+         SuspendLayout();
+         // 
+         // FormOne
+         // 
+         AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         ClientSize = new System.Drawing.Size(800, 450);
+         Name = "FormOne";
+         StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+         Text = "Класс WebClient";
+         ResumeLayout(false);
       }
 
       #endregion
