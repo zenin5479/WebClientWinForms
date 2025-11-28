e037edf99d8a7b240e4bc0c30fc2e8d48b938d53
zenin5479 <zenin5479@gmail.com>
2025-11-28 23:48:24 +0300

    Update 2.0 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -28,26 +28,16 @@
       /// </summary>
       private void InitializeComponent()
       {
-         ListBoxReader = new System.Windows.Forms.ListBox();
          TextBoxReader = new System.Windows.Forms.TextBox();
          SuspendLayout();
          // 
-         // ListBoxReader
-         // 
-         ListBoxReader.FormattingEnabled = true;
-         ListBoxReader.ItemHeight = 15;
-         ListBoxReader.Location = new System.Drawing.Point(12, 12);
-         ListBoxReader.Name = "ListBoxReader";
-         ListBoxReader.Size = new System.Drawing.Size(175, 199);
-         ListBoxReader.TabIndex = 0;
-         // 
          // TextBoxReader
          // 
-         TextBoxReader.Location = new System.Drawing.Point(12, 217);
+         TextBoxReader.Location = new System.Drawing.Point(12, 12);
          TextBoxReader.Multiline = true;
          TextBoxReader.Name = "TextBoxReader";
          TextBoxReader.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-         TextBoxReader.Size = new System.Drawing.Size(175, 132);
+         TextBoxReader.Size = new System.Drawing.Size(379, 337);
          TextBoxReader.TabIndex = 1;
          // 
          // FormOne
@@ -56,7 +46,6 @@
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(403, 361);
          Controls.Add(TextBoxReader);
-         Controls.Add(ListBoxReader);
          Name = "FormOne";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Класс WebClient";
@@ -65,8 +54,6 @@
       }
 
       #endregion
-
-      private System.Windows.Forms.ListBox ListBoxReader;
       private System.Windows.Forms.TextBox TextBoxReader;
    }
 }
