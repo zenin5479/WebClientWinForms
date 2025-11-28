26705bd80b5838b42ceccdd5419f1c16cc8529fb
zenin5479 <zenin5479@gmail.com>
2025-11-28 23:34:25 +0300

    Update 1.9 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WebClientWinForms
 {
