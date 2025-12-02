ee5b0b1e68ae7ef4d609e7d3bf88849e5e3b8d88
zenin5479 <zenin5479@gmail.com>
2025-12-02 10:06:42 +0300

    Update 5.2 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -33,7 +33,7 @@
          ListBoxReader = new System.Windows.Forms.ListBox();
          RichTextBoxReader = new System.Windows.Forms.RichTextBox();
          ButtonWebRequest = new System.Windows.Forms.Button();
-         button1 = new System.Windows.Forms.Button();
+         ButtonClear = new System.Windows.Forms.Button();
          SuspendLayout();
          // 
          // TextBoxReader
@@ -83,21 +83,21 @@
          ButtonWebRequest.UseVisualStyleBackColor = true;
          ButtonWebRequest.Click += ButtonWebRequest_Click;
          // 
-         // button1
+         // ButtonClear
          // 
-         button1.Location = new System.Drawing.Point(669, 12);
-         button1.Name = "button1";
-         button1.Size = new System.Drawing.Size(90, 23);
-         button1.TabIndex = 6;
-         button1.Text = "WebRequest";
-         button1.UseVisualStyleBackColor = true;
+         ButtonClear.Location = new System.Drawing.Point(669, 12);
+         ButtonClear.Name = "ButtonClear";
+         ButtonClear.Size = new System.Drawing.Size(90, 23);
+         ButtonClear.TabIndex = 6;
+         ButtonClear.Text = "WebRequest";
+         ButtonClear.UseVisualStyleBackColor = true;
          // 
          // FormOne
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(771, 500);
-         Controls.Add(button1);
+         Controls.Add(ButtonClear);
          Controls.Add(ButtonWebRequest);
          Controls.Add(RichTextBoxReader);
          Controls.Add(ListBoxReader);
@@ -116,6 +116,6 @@
       private System.Windows.Forms.ListBox ListBoxReader;
       private System.Windows.Forms.RichTextBox RichTextBoxReader;
       private System.Windows.Forms.Button ButtonWebRequest;
-      private System.Windows.Forms.Button button1;
+      private System.Windows.Forms.Button ButtonClear;
    }
 }
