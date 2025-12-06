841a8d30a900185c7a27a42a5d785c1dba9cdbb9
zenin5479 <zenin5479@gmail.com>
2025-12-06 23:04:09 +0300

    Update 16.4 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -46,8 +46,6 @@ namespace WebClientWinForms
                ListBoxReader.Items.Add(line);
                // Прокрутка ListBox вниз
                ListBoxReader.TopIndex = ListBoxReader.Items.Count - 1;
-               // Прокрутка ListBox вниз и выбор последнего элемента
-               //ListBoxReader.SelectedIndex = ListBoxReader.Items.Count - 1;
 
                // RichTextBox
                RichTextBoxReader.AppendText(one);
