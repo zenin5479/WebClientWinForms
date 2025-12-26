9ae4bab666e15776393bbb4266a55d1d044ea5a4
zenin5479 <zenin5479@gmail.com>
2025-12-26 13:31:21 +0300

    Update 18.7 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -15,7 +15,7 @@ namespace WebClientWinForms
       private void ButtonWebClient_Click(object sender, EventArgs e)
       {
          WebClient client = new WebClient();
-         Stream scanner = client.OpenRead("http://127.0.0.1:8888/connection/");
+         Stream scanner = client.OpenRead("http://127.0.0.1:8888/");
          //Stream scanner = client.OpenRead("https://example.com");
          if (scanner != null)
          {
